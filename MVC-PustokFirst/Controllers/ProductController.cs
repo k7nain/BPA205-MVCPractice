bf13cad5014049b1bb2e88598b61eff29bebfa1f
k7nain <murslizadkenan@gmail.com>
2026-04-24 18:44:53 +0400

    MVC-PustokTask (First) created and added

---
+++ MVC-PustokFirst/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_PustokFirst.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC-PustokFirst/Controllers/ShopController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_PustokFirst.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
